d5740c078f1fbf62613e191b7ba3f0f07302a0b5
Ihor <vep2014@ukr.net>
2018-06-10 13:26:48 +0300

    Update DockerBuild

--- DAF/deploy_heroku.sh
+++ DAF/deploy_heroku.sh
@@ -1,5 +1,5 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
-docker build -t  testnetcore21 ./bin/release/netcoreapp2.0/publish
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 heroku container:push web --app $HEROKU_APP_NAME
